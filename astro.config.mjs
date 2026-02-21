e469459280d4f2a08fba801a10d6250526f26451
guzmannicolas <brunonicolasguzman@hotmail.com.ar>
2026-02-21 16:44:24 +1100

    fix(deploy): switch to static output for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
 
   i18n: {
     defaultLocale: 'en',
@@ -23,5 +23,5 @@ export default defineConfig({
   },
 
   integrations: [react()],
-  adapter: vercel()
+  // adapter: vercel()
 });
