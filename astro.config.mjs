845d0f82ae85d68171a8d1f6614fdee841307cc8
guzmannicolas <brunonicolasguzman@hotmail.com.ar>
2026-02-25 10:33:34 +1100

    fix(deploy): switch to vercel server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+
+  adapter: vercel(),
 
   i18n: {
     defaultLocale: 'en',
